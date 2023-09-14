366536da74530bd5969b642a8f3ba06139e5222e
wlrk <as286278@gmail.com>
2023-09-14 22:51:04 +0900

    .

---
+++ Hello/src/Hello.java
@@ -0,0 +1,9 @@
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello, World! ");
+		System.out.print("Hello, World! ");
+		System.out.println(" Difference between println and print");
+	}
+
+}
