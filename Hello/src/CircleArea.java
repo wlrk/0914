694de83c8c4f0022de5def4c9f564144e0139da1
user <user@DESKTOP-DU01E7K>
2023-09-15 10:17:52 +0900

    0915 실습내용

---
+++ Hello/src/CircleArea.java
@@ -0,0 +1,12 @@
+
+public class CircleArea {
+	public static void main (String[] args) {
+		final double PI = 3.14; //원주율을 상수로 선언
+
+		double radius = 10.0;
+		double circleArea = radius*radius*PI;
+
+		System.out.println("원의 면적 = " + circleArea);
+	}
+
+}
--- Hello/src/Hello.java
+++ Hello/src/Hello.java
@@ -1,9 +1,18 @@
 
 public class Hello {
-	public static void main(String[] args) {
-		System.out.println("Hello, World! ");
-		System.out.print("Hello, World! ");
-		System.out.println(" Difference between println and print");
+	public static int sum(int n, int m) {
+			return n + m;
 	}
 
+	public static void main(String[] args) {
+		int i = 20;
+		int s;
+		char a;
+
+		s = sum(i, 10);
+		a= '?';
+		System.out.println(a);
+		System.out.println("Hello");
+		System.out.println(s);
+	}
 }
