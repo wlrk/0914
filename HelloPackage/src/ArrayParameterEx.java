3727e6deec0994b35858649bc679bb095606e601
user <user@DESKTOP-DU01E7K>
2023-10-06 11:56:57 +0900

    1006 실습내용_추가로 다시 해보기

---
+++ HelloPackage/src/ArrayParameterEx.java
@@ -0,0 +1,18 @@
+public class ArrayParameterEx {
+	static void replaceSpace(char a[]) {
+		//write code that replaces whitespace to comma
+	}
+	
+	static void printCharArray(char a[]) {
+		//write code that prints char in char []
+	}
+	
+	public static void main (String[] args) {
+		char c[] = {'T', 'h', 'i','s', ' ', 'a', ' ' , 'p', 'e', 'n', 'c', 'i', 'l', '.'};
+		
+		printCharArray(c);
+		replaceSpace(c);
+		printCharArray(c);
+	}
+
+}
