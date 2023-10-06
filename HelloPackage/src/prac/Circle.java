23d018524cb2ab57d9e59cfc215e2b762b6937e7
user <user@DESKTOP-DU01E7K>
2023-10-06 11:35:39 +0900

    1006 실습내용 circle 다양한 방법으로 불러오기

--- HelloPackage/src/prac/Circle.java
+++ HelloPackage/src/prac/Circle.java
@@ -1,13 +1,52 @@
 package prac;
 
 public class Circle {
-	private final double PI = 3.15 ;
+	private final double PI = 3.14 ;
 	
-	public int radius;
-	public String name;
+	private int radius;     //
+	private String name;     //
+	
+	/*public int radius;
+	public String name; */
 	
 	public Circle() {
+		this(1, "random pizza");
 		
+	}
+	
+	public Circle(Circle c) {
+		this.radius = c.radius;
+		this.name = new String(c.name);
+	}
+	
+	public Circle(int r, String n) {
+		this.initialize(r, n);
+	}
+	
+	private void initialize(int r, String n) {
+		this.radius =  r;
+		this.name = n;
+	}      //private
+	
+	/*public void initialize(int r, String n) {
+		this.radius =  r;
+		this.name = n;
+	}*/
+	
+	public void setRadius(int radius) {
+		this.radius = radius;
+	}
+	
+	public int getRadius() {
+		return this.radius;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
 	}
 	
 	public double getPI() {
@@ -18,8 +57,45 @@ public class Circle {
 		return PI * radius * radius;
 	} //입력한 radius가 여기에 접근해서 원의 면적을 구함
 	
+	public String toString() {
+		return "name : " + getName() + " radius : " + getRadius();
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Class and Object Practice");
+		
+		/*
+		//creating Circle object
+		Circle pizza = new Circle();
+		
+		//initialization using reference variable
+		pizza.radius = 10;
+		pizza.name = "Cheese Pizza";
+		
+		//test
+		System.out.println(pizza.toString());
+		
+		//code for initialization using method
+		Circle pizza2 = new Circle();
+		
+		pizza2.initialize(8, "Chicago Pizza");
+		
+		//test
+		System.out.println(pizza2.toString());
+		
+		//code for initialize using constructor
+		Circle pizza3 = new Circle(15, "Hawaian Pizza");
+		System.out.println(pizza3.toString());*/
+		
+		//code for initialize using constructor
+		Circle pizza = new Circle(15, "Hawaian Pizza");     //
+		System.out.println(pizza.toString());     //
+		
+		Circle pizzaCopy = new Circle(pizza);     //
+		System.out.println(pizzaCopy.toString());     //
 	}
 
 }
+
+
+//주석처리 한 것끼리 출력 값 내고 싶으면 '//' 달린 것들 주석처리 하기
